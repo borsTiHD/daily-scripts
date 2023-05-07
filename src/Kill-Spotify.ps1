236c55ab01d62debed13dc84b637470e0c9a4bb8
borsTiHD <bastian.j@gmx.net>
2023-05-07 10:23:38 +0200

    ✨ Add >> Adding the first script, that kills all running spotify processes.

---
+++ src/Kill-Spotify.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+    This script kills all Spotify processes.
+.DESCRIPTION
+    If you have multiple Spotify processes running, this script will kill all of them.
+.PARAMETER ParameterName
+    ...
+.INPUTS
+    ...
+.OUTPUTS
+    ...
+.EXAMPLE
+    PS C:\> .\Kill-Spotify.ps1
+    This example shows how to use the script.
+.NOTES
+    For better usage, you can create a shortcut to the script and run it like this:
+    powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File "C:\Path\to\Kill-Spotify.ps1"
+.LINK
+    https://github.com/borsTiHD/daily-scripts
+#>
+
+# Get all running Spotify processes
+$processes = Get-Process | Where-Object { $_.ProcessName -like "Spotify*" }
+
+# Kill all Spotify processes
+foreach ($process in $processes) {
+    $process.Kill()
+}
