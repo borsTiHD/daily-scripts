743bf65fc6fbd4b231ddc806296aab3f5ec75654
borsTiHD <bastian.j@gmx.net>
2023-05-07 10:28:44 +0200

    📝 Spotify >> Adding more to the notes.

--- src/Kill-Spotify.ps1
+++ src/Kill-Spotify.ps1
@@ -13,7 +13,7 @@
     PS C:\> .\Kill-Spotify.ps1
     This example shows how to use the script.
 .NOTES
-    For better usage, you can create a shortcut to the script and run it like this:
+    For better usage, you can create a shortcut, or a batch file (.bat) to the script and run it like this:
     powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File "C:\Path\to\Kill-Spotify.ps1"
 .LINK
     https://github.com/borsTiHD/daily-scripts
