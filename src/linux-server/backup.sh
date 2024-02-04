93e02bac8526c700c7bc9991107065a74e77c234
borsTiHD <bastian.j@gmx.net>
2024-02-04 15:50:23 +0100

    feat: ✨ CLeanup backup script and adding new wip features

--- src/linux-server/backup.sh
+++ src/linux-server/backup.sh
@@ -29,7 +29,10 @@ excluded_items=(
 ftp_server="ftp.example.com"
 ftp_user="username"
 ftp_password="password"
-ftp_directory="/backup"
+ftp_directory="/backup/freaks4posts"
+
+# Number of latest backups to keep on FTP server
+num_backups_to_keep=8
 
 # Telegram bot settings
 telegram_bot_token="YOUR_TELEGRAM_BOT_TOKEN"
@@ -41,36 +44,82 @@ send_telegram_notification() {
     curl -s -X POST https://api.telegram.org/bot$telegram_bot_token/sendMessage -d "chat_id=$telegram_chat_id" -d "text=$message"
 }
 
-# Create temporary directory for storing the backup
-tmp_dir=$(mktemp -d)
-trap "rm -rf $tmp_dir" EXIT
+# Function for the backup process
+perform_backup() {
+    local ftp_server="$1"
+    local ftp_user="$2"
+    local ftp_password="$3"
+    local ftp_directory="$4"
 
-# Create archive filename
-day=$(date +%Y-%m-%d)
-hostname=$(hostname -s)
-archive_file="$hostname-$day.tar.gz"
+    # Create temporary directory for storing the backup
+    local tmp_dir=$(mktemp -d)
+    trap "rm -rf $tmp_dir" EXIT
 
-# Construct backup file paths
-backup_files=$(printf "%s " "${backup_directories[@]}")
+    # Create archive filename
+    local day=$(date +%Y-%m-%d)
+    local hostname=$(hostname -s)
+    local archive_file="$hostname-$day.tar.gz"
 
-# Construct exclude arguments for tar
-exclude_args=""
-for item in "${excluded_items[@]}"; do
-    exclude_args+="--exclude=$item "
-done
+    # Construct backup file paths
+    local backup_files=$(printf "%s " "${backup_directories[@]}")
 
-# Print start status message
-echo "Backing up $backup_files to FTP server: $ftp_server"
-date
-echo
+    # Construct exclude arguments for tar
+    local exclude_args=""
+    for item in "${excluded_items[@]}"; do
+        exclude_args+="--exclude=$item "
+    done
 
-# Backup the files using tar to a temporary directory
-tar $exclude_args -zcf "$tmp_dir/$archive_file" "${backup_directories[@]}"
+    # Print start status message
+    echo "Backing up $backup_files to FTP server: $ftp_server"
+    date
+    echo
 
-# Upload the backup to FTP server
-curl -s -T "$tmp_dir/$archive_file" ftp://$ftp_user:$ftp_password@$ftp_server/$ftp_directory/ && \
+    # Backup the files using tar to a temporary directory
+    tar $exclude_args -zcf "$tmp_dir/$archive_file" "${backup_directories[@]}" || { echo "Error: Failed to create backup archive."; return 1; }
+
+    # Upload the backup to FTP server
+    curl -s -T "$tmp_dir/$archive_file" ftp://$ftp_user:$ftp_password@$ftp_server/$ftp_directory/ || { echo "Error: Failed to upload backup to FTP server."; return 1; }
+
+    return 0
+}
+
+# Function to cleanup old backu
+ps on FTP server, keeping only the newest x backups
+cleanup_old_backups() {
+
+    # TODO: Implement cleanup using lftp - NOT WORKING RIGHT NOW!!!
+
+    local ftp_server="$1"
+    local ftp_user="$2"
+    local ftp_password="$3"
+    local ftp_directory="$4"
+    local num_backups_to_keep="$5"
+
+    echo "Connecting to FTP server to cleanup old backups..."
+    lftp -c "open -u $ftp_user,$ftp_password $ftp_server; cd $ftp_directory; ls -t | tail -n +$((num_backups_to_keep + 1)) | xargs -I {} rm {}" || { echo "Error: Failed to cleanup old backups on FTP server."; return 1; }
+
+#     # Connect to FTP server
+#     echo "Connecting to FTP server to cleanup old backups..."
+#     ftp -inv $ftp_server <<EOF
+#     user $ftp_user $ftp_password
+#     cd $ftp_directory
+#     ls -t | awk "NR>$num_backups_to_keep" | while read filename; do
+#         echo "Deleting old backup: $filename"
+#         rm $filename
+#     done
+#     bye
+# EOF
+}
+
+# Perform backup
+perform_backup "$ftp_server" "$ftp_user" "$ftp_password" "$ftp_directory" && \
     send_telegram_notification "[F4P] - Backup uploaded successfully - [✅]" || \
     send_telegram_notification "[F4P] - Failed to upload backup - [❌]"
+
+# Cleanup old backups on FTP server, keeping only the newest x backups
+# cleanup_old_backups "$ftp_server" "$ftp_user" "$ftp_password" "$ftp_directory" "$num_backups_to_keep" && \
+#     send_telegram_notification "[F4P] - Old backups cleanup successful - [✅]" || \
+#     send_telegram_notification "[F4P] - Failed to cleanup old backups - [❌]"
 
 # Print end status message
 echo
